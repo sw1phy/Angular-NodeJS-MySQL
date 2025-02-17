bb989db201371889115f1522b3141971a3c6dee6
unknown <midgardsaidamador@gmail.com>
2025-02-17 15:54:04 -0600

    Added candidate-service and candidate-web as submodules

---
+++ candidate-web/.angular/cache/18.2.14/candidate-web/vite/deps/@angular_material_button.js
@@ -0,0 +1,32 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-K642AMJB.js";
+import "./chunk-U5US367O.js";
+import "./chunk-AXTJT6MY.js";
+import "./chunk-OBSS4RRM.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
